3ad98ec8fd5fea716bdd87046b4555f7155dd77a
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-09-17 19:16:30 +0600

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <main className="flex justify-center items-center min-h-screen bg-sky-200">
+      <div className="w-full max-w-screen-xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg">
+        <h1 className="text-2xl text-green-500 font-bold">Home page</h1>
+      </div>
+    </main>
+  );
 }
