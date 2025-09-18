3578ab8d80eef88d44bef7a9fc425f6eea03479a
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-09-18 22:04:26 +0600

    added filtering photo and vector image

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,11 +1,16 @@
+"use client"
 import React from "react";
 import { data } from "@/lib/db";
 import { Card } from "./Card";
+import { useSearchParams } from "next/navigation";
 
 export const Gallery = () => {
+  const params =useSearchParams()
+  const category =params.get("category")
+  const images =data.filter((item)=>category && item.category === category)
   return (
     <div className="pt-10 grid grid-cols-1 lg:grid-cols-3 gap-5">
-      {data.map((item) => (
+      {images.map((item) => (
         <Card key={item.id} {...item} />
       ))}
     </div>
